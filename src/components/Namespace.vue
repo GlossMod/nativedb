5a7a78e6df6d349d4989d1833e53a8a4b117b0a3
小莫 <xiaomo1433675239@outlook.com>
2024-11-06 14:10:02 +0800

    添加子类的折展功能

--- src/components/Namespace.vue
+++ src/components/Namespace.vue
@@ -12,15 +12,30 @@ const count = computed(() => {
     return Object.keys(props.ns).length
 })
 
-const collapse = ref([])
+const collapse = ref<string[]>([])
+
+function collapseAll() {
+    collapse.value = []
+}
+
+function expandAll() {
+    collapse.value = Object.keys(props.ns)
+}
 
 </script>
 <template>
     <el-collapse-item :title="`${ns_name}[${count}]`">
         <v-lazy :options="{ 'threshold': 0.5 }" transition="fade-transition">
             <el-card>
+                <template #header>
+                    <div class="button">
+                        <el-button link type="primary" @click="collapseAll">全部折叠</el-button>
+                        <el-button link type="primary" @click="expandAll">全部展开</el-button>
+                    </div>
+                </template>
                 <el-collapse v-model="collapse" multiple>
-                    <NativeName v-for="(na, key) in ns" :key="key" :na="na" :ns="ns" :ns_name="ns_name" :na_key="key">
+                    <NativeName v-for="(na, key) in ns" :key="key" :na="na" :ns="ns" :ns_name="ns_name" :na_key="key"
+                        :name="key">
                     </NativeName>
                 </el-collapse>
             </el-card>
@@ -34,12 +49,7 @@ export default {
 }
 </script>
 <style lang='less' scoped>
-li {
-    &:hover {
-        a {
-            color: cadetblue;
-            padding-left: 2px;
-        }
-    }
+.button {
+    text-align: right;
 }
 </style>
